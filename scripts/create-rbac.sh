cef996f985ccb9c5443d3c87d60952476978cdc4
David Young <davidy@funkypenguin.co.nz>
2020-05-07 15:03:06 +1200

    Update template

--- scripts/create-rbac.sh
+++ scripts/create-rbac.sh
@@ -19,9 +19,9 @@ then
   usage "No service specified"
 fi
 
-if [[ ! $service =~ ^[-a-z]{3,20}$ ]]
+if [[ ! $service =~ ^[-a-z]{3,30}$ ]]
 then
-  usage "Service ${service} invalid. Username can only be lower-case letters, 3-20 characters in length"
+  usage "Service ${service} invalid. Username can only be lower-case letters, 3-30 characters in length"
 fi
 
 echo "Creating namespace and RBAC config for ${service}"
@@ -34,13 +34,13 @@ metadata:
 apiVersion: v1
 kind: ServiceAccount
 metadata:
-  name: sa-${service}
+  name: helm-deploy-${service}
   namespace: ${service}
 ---
 kind: Role
 apiVersion: rbac.authorization.k8s.io/v1beta1
 metadata:
-  name: role-${service}
+  name: helm-deploy-${service}
   namespace: ${service}
 rules:
 - apiGroups: ["", "extensions", "apps", "rbac.authorization.k8s.io", "apiextensions.k8s.io", "networking.k8s.io" ]
@@ -56,18 +56,21 @@ rules:
 kind: RoleBinding
 apiVersion: rbac.authorization.k8s.io/v1beta1
 metadata:
-  name: rb-${service}
+  name: helm-deploy-${service}
   namespace: ${service}
 subjects:
 - kind: ServiceAccount
-  name: sa-${service}
-  namespace: ${service}
+  name: helm-deploy-${service}
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: Role
-  name: role-${service}
+  name: helm-deploy-${service}
+
 EOF
 
-# Create the kubeconfig
+# Now apply any other RBAC required (this can vary on a per-chart basis, depending on how much access the service needs)
+kubectl apply -f rbac/*
 
+# Create the kubeconfig
+echo -e "\n\nHere's the kubeconfig for the service account we just created:\n\n"
 scripts/create-kubeconfig.sh ${service} --namespace ${service}
